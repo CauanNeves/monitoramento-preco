3f98150ba1d26a64f4e9e25e99e2d9edd0cc07fe
Cauan Neves <c.neves8903@gmail.com>
2025-01-08 22:19:06 -0300

    0.1

---
+++ app.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from time import sleep
+import os
+
+#Funcções do Webdriver
+def start_driver():
+    chrome_options = Options()
+    arguments = ['--lang=pt-BR', '--start-maximized', '--incognito']
+
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.default_directory': 'C:\\Users\\secretario',
+        'download.directory_upgrade': True,
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1,
+    })
+
+    driver = webdriver.Chrome(options=chrome_options)
+
+    return driver
+
+def wait_for_element(driver, by, value, timeout=60):
+    return WebDriverWait(driver, timeout).until(
+        EC.presence_of_element_located((by, value))
+    )
+
+#Função principal
+def main():
+    driver = start_driver()
+    driver.get('https://www.terabyteshop.com.br/produto/31959/gabinete-gamer-redragon-wideload-extreme-mid-tower-rgb-vidro-curvado-temperado-atx-black-sem-fonte-sem-fan-ca-605b')
+    sleep(0.5)
+    price_in_cash = driver.execute_script(f'''
+return document.evaluate(
+    '//p[@id= "valVista"]', 
+    document, 
+    null, 
+    XPathResult.FIRST_ORDERED_NODE_TYPE, 
+    null
+    ).singleNodeValue;
+                                    ''')
+    driver.execute_script('arguments[0].scrollIntoView({behavior: "instant", block: "center"});', price_in_cash)
+    price_text = driver.execute_script('return arguments[0].innerText;', price_in_cash)
+    print(f'Preço à vista: {price_text}')
+    
+if __name__ == '__main__':
+    main()
